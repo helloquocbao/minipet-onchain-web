4c494456106dff08e70fa17670dcf6c2ea7d9e58
QBao <lehoquocbao9@gmail.com>
2026-06-20 08:47:31 +0700

    feat: update blockchain configuration and enhance pet detail page with type and description fields

--- src/services/blockchain/sui.ts
+++ src/services/blockchain/sui.ts
@@ -1,10 +1,10 @@
 import { getJsonRpcFullnodeUrl as getFullnodeUrl, SuiJsonRpcClient as SuiClient } from '@mysten/sui/jsonRpc';
 
-export const PACKAGE_ID = process.env.NEXT_PUBLIC_PACKAGE_ID || '0x335f115a0189a9ec083080e1aff68d88b740b362f5499e8aa9d706a0ca14f877';
-export const PET_TOKEN_PACKAGE_ID = process.env.NEXT_PUBLIC_PET_TOKEN_PACKAGE_ID || '0x46af6cc67f8a40f6a4a5267087176e6e4341e51df6e9decabfe07cf606186e23';
+export const PACKAGE_ID = process.env.NEXT_PUBLIC_PACKAGE_ID || '0x1de01a3d4bf50fdc2710f765313469316efac4252a3192f835d71ff225578c03';
+export const PET_TOKEN_PACKAGE_ID = process.env.NEXT_PUBLIC_PET_TOKEN_PACKAGE_ID || '0x7762d89a5c01c00ae0d118e3a2f6191ef13aa701a5aa7f57ecc38fe6959c403e';
 export const TREASURY_CAP_ID = process.env.NEXT_PUBLIC_TREASURY_CAP_ID || '0x8c0387f8bf0654f9abef3690c5289226c06c15c3ea2d5a0a3451c891600bf31f';
-export const GLOBAL_CONFIG_ID = process.env.NEXT_PUBLIC_GLOBAL_CONFIG_ID || '0xe3a081bf02478822daa3b8e957f2b6c10bacdf7c33ebd2b1db6e4f272e329886';
-export const ADMIN_CAP_ID = process.env.NEXT_PUBLIC_ADMIN_CAP_ID || '0x24ed356d9227ec6d8fdefdbd46f8aa4b7dad951c7e499be290028a3e38baea36';
+export const GLOBAL_CONFIG_ID = process.env.NEXT_PUBLIC_GLOBAL_CONFIG_ID || '0xc77d430ff60cb762958bd36f7053d42a43bc6562fb21855b67fb8ff764dadf83';
+export const ADMIN_CAP_ID = process.env.NEXT_PUBLIC_ADMIN_CAP_ID || '0x092031bc999896a437468675d8600288dfa48fd646444b07a769191236d9590d';
 export const PET_TOKEN_TYPE = `${PET_TOKEN_PACKAGE_ID}::pet_token::PET_TOKEN`;
 
 const network = (process.env.NEXT_PUBLIC_SUI_NETWORK as 'testnet' | 'mainnet' | 'devnet') || 'testnet';
